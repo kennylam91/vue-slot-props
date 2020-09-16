e531730b1888c3ff51e78e465afe383cfea51f85
kennylam91 <phamvanlam1991@gmail.com>
2020-09-16 17:14:42 +0700

    commit

---
+++ src/components/CurrentUser.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span>
+      <slot :user="user" :action="action">
+        {{user.lastName}}
+      </slot>
+    </span>
+    
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      user:{
+        firstName: 'pham',
+        lastName:'lam'
+      }, 
+      action:{
+        first: 'punch'
+      }
+    }
+  },
+}
+</script>
